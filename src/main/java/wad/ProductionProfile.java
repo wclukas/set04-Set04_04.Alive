641f6841944ee74667cddd8d9cd069536c95a5ea
Lukas <lukas.wilenius@helsinki.fi>
2017-11-30 15:26:09 +0200

    production profile

--- src/main/java/wad/ProductionProfile.java
+++ src/main/java/wad/ProductionProfile.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package wad;
 
-/**
- *
- * @author lucke
- */
+
+import javax.sql.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("production")
 public class ProductionProfile {
-    
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
